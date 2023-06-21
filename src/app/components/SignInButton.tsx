64c26b4c99f36a8ef1ced0aa685c09fe11db4694
Jom Dollesin <codewithjom@gmail.com>
2023-06-22 07:43:33 +0800

    feat: added a sign in button.

---
+++ src/app/components/SignInButton.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+export default function SignInButton() {
+  const { data: session } = useSession()
+
+  if (session && session.user) {
+    return (
+      <div>
+        <p>{session.user.name}</p>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </div>
+    )
+  }
+
+  return <button onClick={() => signIn()}>Sign In</button>
+}
